58196ab4dbcddefe062a691938c252af56134dfa
crwlos <carloserocha27@gmail.com>
2020-08-04 23:31:22 -0300

    Router: Adicionado handler para as rotas

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	boom "github.com/darahayes/go-boom"
 	authentication "github.com/carloserocha/history-application/authentication"
 	scavenger "github.com/carloserocha/history-application/scavenger"
 	"github.com/gorilla/mux"
@@ -17,6 +18,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(false)
 	router.HandleFunc("/scavenger", scavenger.CreateScavenger).Methods("POST")
 	router.HandleFunc("/login", authentication.AuthenticateLogin).Methods("POST")
+
+	router.Use(boom.RecoverHandler)
 
 	log.Fatal(http.ListenAndServe(":8001", router))
 }
